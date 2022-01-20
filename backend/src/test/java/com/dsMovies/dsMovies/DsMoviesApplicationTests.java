306ec29b83eaa9967be1cd687c74e21dce627051
marciogrecco <marcio.greco@outlook.com>
2022-01-20 10:38:45 -0300

    Project Created

---
+++ backend/src/test/java/com/dsMovies/dsMovies/DsMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsMovies.dsMovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
